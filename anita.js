2792f708a1fe43e03ca1dc2ce39add605c731fc6
Kevin-Modif-BOTS <125774234+kevin-BOT-OFC@users.noreply.github.com>
2023-05-10 10:29:51 -0500

    new

--- anita.js
+++ anita.js
@@ -1148,6 +1148,47 @@ await delay(5000)
 enviar(`${pushname} Su porcentaje de puto(@) es de  : ${aletq}%`)
 break
 
+case  'casino':
+a = '🍇'
+b = '🍎'
+c = '🍓'
+e = '🍑'
+f = '💰'
+g = '🥝'
+pw = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck = pw[Math.floor(Math.random() * pw.length)]
+pw1 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck1 = pw1[Math.floor(Math.random() * pw1.length)]
+pw2 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck2 = pw2[Math.floor(Math.random() * pw2.length)]
+pw3 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck3 = pw3[Math.floor(Math.random() * pw3.length)]
+pw4 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck4 = pw4[Math.floor(Math.random() * pw4.length)]
+pw5 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck5 = pw5[Math.floor(Math.random() * pw5.length)]
+pw6 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck6 = pw6[Math.floor(Math.random() * pw6.length)]
+pw7 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck7 = pw7[Math.floor(Math.random() * pw7.length)]
+pw7 = [`${a}`, `${b}`, `${c}`, `${e}`, `${f}`, `${g}`]
+luck7 = pw7[Math.floor(Math.random() * pw7.length)]
+s = `┃ │  ${luck} │  ${luck1} │ ${luck2}`
+a = `┃ │  ${luck3} │  ${luck4} │ ${luck5}`
+m = `┃ │  ${luck6} │  ${luck7} │ ${luck7}`
+u = `
+╭──╼┥𝐂𝐀𝐒𝐈𝐍𝐎┝╾──╮
+╽ ┌──────────┐ ┃
+${s}
+┃ ├──────────┤ ┃
+${a}
+┃ ├──────────┤ ┃
+${m}
+╿ └──────────┘ ╿
+╰─┨⃞ 𝐂𝐀𝐒𝐒𝐈𝐍𝐎┠─╯`
+enviar(`${u}`)
+break
+
 
 
 //la verdad no c//
